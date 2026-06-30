bb56c6f71e021d9689877e30644cdb38a289abc0
dimasim <officialdimasim@gmail.com>
2026-06-30 16:01:42 +0200

    feat: add .gitignore, .env.example, and update sample7

--- sample7/main.tf
+++ sample7/main.tf
@@ -1,15 +1,20 @@
-#resource "aws_instance" "my_instance" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
+terraform {
+  required_version = ">= 1.1"
 
-  tags = {
-    Name        = "Praktikum-Instance"
-    Environment = "LocalStack-Sandbox"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74"
+    }
   }
 }
 
-# Output untuk memudahkan verifikasi ID instance setelah deploy
-output "my_instance_id" {
-  value       = aws_instance.my_instance.id
-  description = "ID dari EC2 Instance tiruan di LocalStack"
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
 }
