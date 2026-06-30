0af5d741ee585e9d4090ed7315a69c660f2c116a
Dimas Irsyad Maulana <officialdimasim@gmail.com>
2026-06-30 20:44:18 +0700

    Initial commit

---
+++ sample7/main.tf
@@ -0,0 +1,15 @@
+#resource "aws_instance" "my_instance" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "Praktikum-Instance"
+    Environment = "LocalStack-Sandbox"
+  }
+}
+
+# Output untuk memudahkan verifikasi ID instance setelah deploy
+output "my_instance_id" {
+  value       = aws_instance.my_instance.id
+  description = "ID dari EC2 Instance tiruan di LocalStack"
+}
